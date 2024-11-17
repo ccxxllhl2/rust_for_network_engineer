58b471135b2b2bd16ecd96501441e86532f45794
Jackal <ccxxll23@163.com>
2024-11-17 23:18:18 +0800

    good

--- class/class02_primitives/target/debug/class02_primitives.d
+++
@@ -1,1 +0,0 @@
-/root/rust_for_network_engineer/class/class02_primitives/target/debug/class02_primitives: /root/rust_for_network_engineer/class/class02_primitives/src/main.rs
--- class/class02_primitives/target/debug/deps/class02_primitives-4fa47678f0107ed8.d
+++
@@ -1,5 +0,0 @@
-/root/rust_for_network_engineer/class/class02_primitives/target/debug/deps/class02_primitives-4fa47678f0107ed8: src/main.rs
-
-/root/rust_for_network_engineer/class/class02_primitives/target/debug/deps/class02_primitives-4fa47678f0107ed8.d: src/main.rs
-
-src/main.rs:
